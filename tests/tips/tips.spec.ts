63b1dc03ed484d37679c1e46ce1e172690f0bb07
Lisavetti <lizaemets0244@gmail.com>
2024-10-25 19:14:18 +0300

    parametrized tests

--- tests/tips/tips.spec.ts
+++ tests/tips/tips.spec.ts
@@ -15,4 +15,13 @@ test.describe.only('Tips and tricks section', () => {
         test.fixme(browserName === 'chromium', 'test is not stable, needs for revision');
         await page.goto('https://example.com');
     });
+
+    const people = ['John', 'Jake', 'Ilon', 'Julia', 'Liza'];
+    for (const name of people) {
+        test.only(`Running test for ${name}`, async ({ page }) => {
+            await page.goto('http://zero.webappsecurity.com/');
+            await page.fill('#searchTerm', `${name}`);
+            // await page.waitForTimeout(3000);
+        });
+    };
 })
